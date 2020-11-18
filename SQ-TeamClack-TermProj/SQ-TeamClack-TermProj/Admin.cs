bc04dca8335e3f2a93bdfa0af6c581023d48f21f
SemiDoge <taelforlife@gmail.com>
2020-11-18 18:11:18 -0500

    Started to stub the classes.

--- SQ-TeamClack-TermProj/SQ-TeamClack-TermProj/Admin.cs
+++ SQ-TeamClack-TermProj/SQ-TeamClack-TermProj/Admin.cs
@@ -8,5 +8,42 @@ namespace SQ_TeamClack_TermProj
 {
     class Admin
     {
+        private ulong AdminID;
+        private string firstName;
+        private string lastName;
+        public Admin()
+        {
+
+        }
+
+        public void adminConfig()
+        {
+            //todo adminConfig() logic
+        }
+
+        public void reviewLogFiles()
+        {
+            //todo reviewLogFiles() logic
+        }
+
+        public void modifyRateTables(ulong adminID)
+        {
+            //todo modifyRateTables() logic
+        }
+
+        public void modifyCarrierData(ulong adminID)
+        {
+            //todo modifyCarrierData() logic
+        }
+
+        public void modifyRouteTable(ulong adminID)
+        {
+            //todo modifyRouteTable() logic
+        }
+
+        public void initiateBackup(string backupString)
+        {
+            //todo initiateBackup() logic
+        }
     }
 }
--- SQ-TeamClack-TermProj/SQ-TeamClack-TermProj/Order.cs
+++ SQ-TeamClack-TermProj/SQ-TeamClack-TermProj/Order.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SQ_TeamClack_TermProj
 {
     class Order
     {
+        private ulong OrderID;
+        private Item[] orderManifest; //a better idea for this would be placing it inside of a data structure
+        private DateTime orderDate;
+        private bool markedForAction = false;
+        private string[] tripRoutes;
+        private double totalOrderWeight;
+        private string carrierData;
+        private double fees;
+
+        public Order()
+        {
+
+        }
+
+        private void calculateWeight()
+        {
+            //todo calculateWeight() logic
+
+            //totalOrderWeight = ;
+        }
+
+        private DateTime getTime()
+        {
+            DateTime rDt = new DateTime();
+
+            //todo getTime() logic
+
+            return rDt;
+        }
+
+        public string queryOrder()
+        {
+            StringBuilder strBr = new StringBuilder();
+
+            //todo queryOrder() logic
+
+            return strBr.ToString();
+        }
+
+        public string queryManifest()
+        {
+            StringBuilder strBr = new StringBuilder();
+
+            //todo queryManifest() logic
+
+            return strBr.ToString();
+        }
+
+        public void recalculateWeight()
+        {
+            //todo recalculateWeight() logic
+        }
+
+        public void calculateSubTotal()
+        {
+            //todo calculateSubTotal() logic
+        }
+
     }
 }
