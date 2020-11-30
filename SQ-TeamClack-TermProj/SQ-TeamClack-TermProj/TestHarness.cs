cc2b4895deb007b28041fa57e33566a30774f04c
Tommy Ngo <TommyKhoaNgo@live.com>
2020-11-29 21:40:01 -0500

    Added Test Harness for MySql

---
+++ SQ-TeamClack-TermProj/SQ-TeamClack-TermProj/TestHarness.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SQ_TeamClack_TermProj
+{
+    class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection connection = new MySqlConnection("server=localhost; port=3306; UID=root; PASSWORD=E3e25475515!; DATABASE=Omnicorp");
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Items;");
+            connection.Open();
+
+            cmd.Connection = connection;
+
+            string x = cmd.ExecuteScalar().ToString();
+
+
+            connection.Close();
+
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SQ-TeamClack-TermProj/TestProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection connection = new MySqlConnection("server=localhost; port=3306; UID=root; PASSWORD=E3e25475515!; DATABASE=Omnicorp");
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Items;");
+            connection.Open();
+
+            cmd.Connection = connection;
+
+            MySqlDataReader myReader = cmd.ExecuteReader();
+            StringBuilder sb = new StringBuilder();
+            myReader.Read();
+            for (int i = 0; i < 6; i++)
+            {
+                sb.Append(myReader[i].ToString());
+                i++;
+            }
+
+
+            connection.Close();
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
