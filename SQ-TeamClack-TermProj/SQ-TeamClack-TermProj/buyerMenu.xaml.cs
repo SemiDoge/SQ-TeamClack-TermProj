829449bf3671b57ad56284211acdff124521de51
SemiDoge <taelforlife@gmail.com>
2020-12-08 01:11:59 -0500

    Added buyerMenu back to project.

---
+++ SQ-TeamClack-TermProj/SQ-TeamClack-TermProj/buyerMenu.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQ_TeamClack_TermProj
+{
+    /// <summary>
+    /// Interaction logic for buyerMenu.xaml
+    /// </summary>
+    public partial class buyerMenu : Page
+    {
+        User localUser;
+        public buyerMenu(User localUser)
+        {
+            InitializeComponent();
+            UsernameLabel.Content = localUser.USERNAME;
+
+        }
+
+        private void logoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Return to login page
+            loginPage login = new loginPage(localUser);
+            this.NavigationService.Navigate(login);
+
+        }
+
+        private void InitiateOrderBTN_Click(object sender, RoutedEventArgs e)
+        {
+            // Go to initiate order page
+            buyer_InitiateOrder initiateOrder = new buyer_InitiateOrder(localUser);
+            this.NavigationService.Navigate(initiateOrder);
+        }
+
+        private void ReviewCustomersBTN_Click(object sender, RoutedEventArgs e)
+        {
+            // Go to Review Customers page
+            buyer_ReviewCustomers reviewCustomers = new buyer_ReviewCustomers(localUser);
+            this.NavigationService.Navigate(reviewCustomers);
+        }
+
+        private void ReviewOrdersBTN_Click(object sender, RoutedEventArgs e)
+        {
+            // Go to Review Orders page
+            buyer_ReviewOrders reviewOrders = new buyer_ReviewOrders(localUser);
+            this.NavigationService.Navigate(reviewOrders);
+        }
+    }
+}
